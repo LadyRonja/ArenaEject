243c9bbdc3afd274b64fb27d551367295ddf4670
Ronja <ronja_mattsson@outlook.com>
2024-05-16 13:45:41 +0200

    minor graphical effects

--- Assets/Scripts/CodeBasedVisualEffects/Hover.cs
+++ Assets/Scripts/CodeBasedVisualEffects/Hover.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Hover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float range = 0.3f;
+    [SerializeField] float speed = 5f;
+    [SerializeField] bool randomStart = true;
+    float randomOffSet = 0;
+    Vector3 startPos;
+
+    private void Start()
     {
+        startPos = transform.position;
+        if(randomStart)
+        {
+            randomOffSet = Random.Range(-range, range);
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float yModifier = range * Mathf.Sin((Time.time + randomOffSet) * speed);
+        this.transform.position += new Vector3(0, yModifier, 0) * Time.deltaTime;
     }
 }
