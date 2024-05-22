a415d497792cecec19a44e489d76f2f74934aac8
RJ <rojona@users.noreply.github.com>
2024-05-22 15:34:32 +0200

    DevJoel scene now adapted for the end game buttons
    Next button is currently adapted to one, two or three scenes in build settings. Scene 0 should be the menu scene in all cases (so the Menu button works.) If there's only one level scene (at index 1) it reloads. If there's two level scenes the currently non-active scene loads.

--- Assets/Scripts/GameManagement/KillPlane.cs
+++ Assets/Scripts/GameManagement/KillPlane.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Threading;
 using DG.Tweening;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
